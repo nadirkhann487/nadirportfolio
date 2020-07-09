2044af1d18c214d802e1f64424f27d829d2ee081
Nadir Khan <nadirkhann487@gmail.com>
2020-07-09 19:37:39 +0100

    scripts.js updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("scripts")
+require("scripts.js")
 import "bootstrap";
 
 global.$ = jQuery;
