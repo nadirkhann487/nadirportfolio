eb67dcae8c5f2006a8cba6ecd217f56c79525103
Nadir Khan <nadirkhann487@gmail.com>
2020-07-09 21:48:12 +0100

    modal fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
   def index
   	@skills = Skill.all
   	@items = PortfolioItem.all
-  	@item1 = PortfolioItem.find(1)
-  	@item2 = PortfolioItem.find(2)
+  	@item1 = PortfolioItem.find(2)
+  	@item2 = PortfolioItem.find(1)
   end
 
 end
