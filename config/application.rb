885254aa09af24eb863aed61830ef8bf6468d98f
Nadir <nadir@homeflow.co.uk>
2022-10-25 15:07:35 +0100

    require_master_key = false

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module PortfolioApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.require_master_key = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
